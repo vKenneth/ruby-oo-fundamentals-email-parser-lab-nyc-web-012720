05804062ea48ea69ba71ccee25a849b727bc6b27
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-29 19:01:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,16 @@
-# Build a class EmailParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+class EmailAddressParser
+  attr_reader :email
+
+  def initialize(emails)
+    @emails = emails
+  end
+  
+# def parse
+#     emails.split(/, | /).uniq
+#   end
+# end
+
+email_address = "Kenneth_Valdivia@Yahoo.com"
+parser = EmailAddressParser.new(Kenneth_Valdivia@Yahoo.com)
+parser.parse
+mail = Mail.read('/path/to/.eml')
