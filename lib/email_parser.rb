aecdad5ca8948f967f90d8ff6ec092b10434a820
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-29 19:13:47 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,11 +1,11 @@
 class EmailAddressParser
   attr_reader :email
-../lib/email_parser
+
   def initialize(emails)
     @emails = emails
   end
   
- email_addresses = "kenneth_valdivia@yahoo.com"
+ email_addresses = "../lib/email_parser"
  
  parser = EmailAddressParser.new(kenneth_valdivia@yahoo.com)
  
